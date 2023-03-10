5fad39b5c176b13213707bec5e52b4a0dc60c977
Nick K <nick.kruchinin@gmail.com>
2023-03-10 15:59:45 +0300

    Make createTransform result read-only

--- src/public/createTransform.tsx
+++ src/public/createTransform.tsx
@@ -16,8 +16,8 @@ export type TransformImplementation<TNode extends Node | Text> =
 export type CreateTransformProps<T extends Node | Text> = NodeProps<T>
 
 export interface NodeTransform<TNode extends Node | Text> {
-  matcher: NodeMatcher<TNode>
-  transform: TransformImplementation<TNode>
+  readonly matcher: NodeMatcher<TNode>
+  readonly transform: TransformImplementation<TNode>
 }
 
 export type LeafTransform<TLeaf extends Text = Text> =
@@ -32,7 +32,7 @@ export type ElementTransform<TElement extends Node = Node> =
 const createTransform = <TNode extends Node | Text>(
   matcher: NodeMatcher<TNode>,
   transform: TransformImplementation<TNode>
-): NodeTransform<TNode> => ({matcher, transform})
+): NodeTransform<TNode> => Object.freeze({matcher, transform})
 
 /**
  * Creates a leaf node transform. It takes `LeafNodeMatcher` as the first argument to match any specific node during `transformNodes` call, and `transform` implementation as the second argument. This transform implementation then will be called for each matched node to create a `ReactElement` for this node.
