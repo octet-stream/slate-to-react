2d0e1bc6f81697fbd935666bf6f04b2268ec6c4d
Nick K <nick.kruchinin@gmail.com>
2024-02-14 22:39:39 +0300

    Move from ts-node to tsx for AVA tests

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,6 @@
 export default {
   failFast: true,
+  workerThreads: false, // Disable to make tsx work
   require: "global-jsdom/register",
   environmentVariables: {
     TS_NODE_PROJECT: "tsconfig.ava.json"
